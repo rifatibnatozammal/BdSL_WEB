c314e4256971f38417513334dc1d7a7f0460604c
rifat <rifat15-5954@diu.edu.bd>
2026-01-03 23:46:46 +0600

    initial deploy clean

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "BdSL Web App is running!"
+
+# Example API route
+@app.route("/health")
+def health():
+    return jsonify({"status": "ok"})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
